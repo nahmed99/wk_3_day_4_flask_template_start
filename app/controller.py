f6a836ff4153e52bad142b66382017e04602d5bc
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-19 10:58:05 +0000

    Started to add templates...

--- app/controller.py
+++ app/controller.py
@@ -1,15 +1,6 @@
+from flask import render_template
 from app import app
 
 @app.route('/') # listen to the home/root
 def index():
-    return '''
-    <html>
-        <head>
-            <title>Home Page My Todos</title>
-        </head>
-        <body>
-            <h1>My Todo List</h1>
-        </body>
-
-    </html>
-    '''
+    return 
