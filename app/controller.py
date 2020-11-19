150bcca62417239cea30959b18de7b2d71768891
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-19 13:55:03 +0000

    added a second page - info.html

--- app/controller.py
+++ app/controller.py
@@ -2,6 +2,11 @@ from flask import render_template
 from app import app
 from app.models.todo_list import tasks
 
-@app.route('/') # listen to the home/root
+@app.route('/') # This listens for 'requests' to the home/root
 def index():
     return render_template('index.html', title="Home", tasks=tasks)
+
+
+@app.route('/info')
+def info():
+    return render_template('info.html', title='info')
