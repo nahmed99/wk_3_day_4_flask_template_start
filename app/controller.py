910b9feba80ec0bb2daff9d66a906d75832f15e2
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-19 11:14:55 +0000

    Added jinja

--- app/controller.py
+++ app/controller.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
+from app.models.todo_list import tasks
 
 @app.route('/') # listen to the home/root
 def index():
-    return render_template('index.html', title="Home")
+    return render_template('index.html', title="Home", task=tasks)
